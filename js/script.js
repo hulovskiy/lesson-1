49c9c7215b47023f48171e5a676450581377edf2
бой <boi@MacBook-Air-2.local>
2023-11-29 17:17:51 +0200

    home work js

--- js/script.js
+++ js/script.js
@@ -5,35 +5,64 @@
 // Скопіюйте значення зі змінної name в city.
 // Виведіть значення змінної city, використовуючи функцію console.log (яка повинна показати “Іван”).
 
+// let user = "Іван";
+// let city = "Khmelnitskiy";
+
+// city = user;
+
+// console.log(city);
+
 //***2***
 //Який буде результат виконання скрипта?
 // let name = "Olga";
-// console.log(`привіт ${1}`); //
-// console.log(`привіт ${"name"}`); //
-// console.log(`привіт ${name}`); // ?
+// console.log(`привіт ${1}`); // привіт 1
+// console.log(`привіт ${"name"}`); // привіт name
+// console.log(`привіт ${name}`); // привіт Olga
 
 //***3***
 //Видобути число зі змінних
 // let a = "5";
 // let b = "13cvb";
 // let c = "12.9sxdcfgv";
-// вивести в консоль тип
+
+// console.log(parseInt(a));
+// console.log(parseInt(b));
+// console.log(parseFloat(c));
+
+// // вивести в консоль тип
+
+// console.log(typeof Number(a));
+// console.log(typeof(b));
+// console.log(typeof(c));
 
 //***4***
 //Зробіть, щоб 0.1 + 0.2 = 0.3
 
+// console.log((0.1*10 + 0.2*10)/10);
+
+
 //***5**
 //Поверніть найбільше число с набору 20, 10, 50, 40
+
+// console.log(Math.max(20, 10, 50, 40));
 
 //***6**
 //Поверніть випадкове число в діапазоні від 2 до 4
 
+// console.log(Math.random() * 4-2+2);
+
 //***7**
 //дізнатись довжину message
+
 // const message = "Welcome to Bahamas!";
+
+// console.log(message.length); //19
 
 //***8**
 //вивести в консоль message  великими літерами
+
+// console.log(message.toUpperCase());
+
 
 //***9**
 // створити пустий об*єкт
@@ -43,5 +72,23 @@
 // додати буль з ключем: like flowers
 // вивести результат в консоль
 
+// let user = {};
+// user.name = 'artem';
+// user.age = 29; 
+// user.city = 'khmelnitskiy';
+
+// delete user.city;
+
+// user['like flowers'] = true;
+
+// console.log(user);
+
+
 //***10**
 // За допомогою циклу  “for…in” вивести в консоль ключі і значення об*єкта
+
+// for (key in user){
+//     console.log(key);
+//     console.log(user[key]);
+
+// }
